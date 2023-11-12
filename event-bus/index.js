0282edf7c9a58ece7f700a8b0355d819227c76db
Nathan Schrader <natecschrader@gmail.com>
2023-11-11 17:02:30 -0800

    add get - /events route

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = []
+
 app.post('/events', (req, res) => {
   const event = req.body
+
+  events.push(event)
 
   // posts
   axios.post('http://localhost:4000/events', event)
@@ -23,6 +27,10 @@ app.post('/events', (req, res) => {
 
   res.send({ status: 'OK' })
 })
+
+app.get('/events', (req, res) => {
+  res.send(events)
+} )
 
 app.listen(4005, () => {
   console.log('Listening on 4005')
